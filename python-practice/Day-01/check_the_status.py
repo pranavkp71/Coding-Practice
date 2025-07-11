cb5f60c511eb4db549165bca8897a086c8e43331
pranav <pranavkp170@gmail.com>
2025-07-11 16:11:52 +0530

    python_practice day-01: Check the Status

---
+++ python-practice/Day-01/check_the_status.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkStatus(self, a, b, flag):
+        if (flag == False and ((a > 0 and b < 0) or (a < 0 and b > 0))):
+            return True
+        elif (flag == True and a < 0 and b < 0):
+            return True
+        else:
+            return False
